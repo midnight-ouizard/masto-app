e4eaf42cc99df41582fad89f04bc21beaf5e0ac4
Jesus Cova <jesuscovam@gmail.com>
2023-10-23 17:44:30 -0600

    fix redirects

--- apps/web/src/hooks.server.ts
+++ apps/web/src/hooks.server.ts
@@ -1,17 +1,14 @@
 import { OATH_TOKEN_KEY } from '$lib/mastodon/auth/oath_code';
 import { redirect, type Handle } from '@sveltejs/kit';
 
-const AUTH_ROUTES = ['/timelines'];
+const AUTH_ROUTES = ['/'];
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const oathToken = event.cookies.get(OATH_TOKEN_KEY);
 
 	if (event.url.pathname === '/login' && oathToken) {
 		console.warn('trigger redirect a home', { url: event.url.pathname });
-		throw redirect(301, '/timelines/home');
-	} else if (event.url.pathname === '/' && oathToken) {
-		console.warn('trigger redirect a home', { url: event.url.pathname });
-		throw redirect(301, '/timelines/home');
+		throw redirect(301, '/');
 	} else if (AUTH_ROUTES.includes(event.url.pathname) && !oathToken) {
 		console.log('trigger redirect a login', { url: event.url.pathname });
 		throw redirect(301, '/login');
